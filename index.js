a9afdf606b2fe99562516894ed44d6eba7082d5f
Nate Nelson <7nater.nelson@gmail.com>
2020-03-31 17:48:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var talent = [];
-  for(let i = 0; i <= musicians.length; i++){
+  for(let i = 0; i < musicians.length; i++){
     talent.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return talent;
